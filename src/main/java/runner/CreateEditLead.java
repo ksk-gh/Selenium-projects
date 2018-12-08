b742100bae85da8ef2af16a4420c472aafdd9f32
SANKARA KARTHIKEYAN <SANKARA KARTHIKEYAN@SANKARA>
2018-12-08 07:51:07 +0530

    sample

--- src/main/java/runner/CreateEditLead.java
+++ src/main/java/runner/CreateEditLead.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/main/java/feature/createeditlead.feature",glue={"pages","steps"},tags={"@Smoke","@Sanity"})
+@CucumberOptions(features="src/main/java/feature/createeditlead.feature",glue={"pages"},tags={"@Smoke","@Sanity"})
 public class CreateEditLead {
 
 }
